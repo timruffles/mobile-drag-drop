884a071c02182f7c72cf243bee6c91c411d77122
Paul Shirren <shirro@shirro.com>
2013-11-06 23:17:59 +1030

    Add bind polyfill for ios5 browsers

--- ios-drag-drop.js
+++ ios-drag-drop.js
@@ -215,4 +215,8 @@
     }), 0) / arr.length;
   }
 
+
+  // Function.bind polyfill for Safari < 5.1.4 and iOS.
+  // From https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+  Function.prototype.bind||(Function.prototype.bind=function(c){if("function"!==typeof this)throw new TypeError("Function.prototype.bind - binding an object that is not callable");var d=Array.prototype.slice.call(arguments,1),e=this,a=function(){},b=function(){return e.apply(this instanceof a?this:c||window,d.concat(Array.prototype.slice.call(arguments)))};a.prototype=this.prototype;b.prototype=new a;return b});
 })(document);
