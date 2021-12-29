f84fac7696149b87dff8593ceaa7f3875737c22f
reppners <stefanreppner@googlemail.com>
2021-12-29 12:30:17 +0100

    include README.md and LICENSE in release

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,7 +65,7 @@ module.exports = function (grunt) {
                         flatten: false
                     },
                     {
-                        src: "package.json",
+                        src: ["package.json", "README.md", "LICENSE"],
                         dest: "release/"
                     }
                 ]
